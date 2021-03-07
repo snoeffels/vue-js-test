f9be82037b59242b1bd1a1118e346e287b54bf13
snoeffels <dennis.borrmann@strivecast.com>
2021-03-07 13:06:42 +0100

    - added login form

--- src/App.vue
+++ src/App.vue
@@ -24,37 +24,22 @@
           width="100"
         />
       </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <LoginForm v-show="!userLoggedIn"/>
+
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import LoginForm from './components/LoginForm';
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+    LoginForm,
+  }
 };
 </script>
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-form v-model="valid">
+    <v-card
+        class="mx-auto"
+        max-width="500"
+    >
+      <v-card-text>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                  v-model="username"
+                  :rules="usernameRules"
+                  label="Username"
+                  required
+              />
+            </v-col>
+
+            <v-col cols="12">
+              <v-text-field
+                  type="password"
+                  v-model="password"
+                  :rules="passwordRules"
+                  label="Password"
+                  required
+              />
+            </v-col>
+          </v-row>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="login"> Login </v-btn>
+      </v-card-actions>
+    </v-card>
+    <v-snackbar v-model="loginFailedSnackbar.show">
+      {{ loginFailedSnackbar.text }}
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="loginFailedSnackbar.show = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data: () => ({
+      valid: false,
+      username: '',
+      password: '',
+      usernameRules: [
+        v => !!v || 'Username is required'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required'
+      ],
+      loginFailedSnackbar: {
+        show: false,
+        text: `Incorrect username or password`,
+      },
+      users: [
+        {
+          username: "user",
+          password: "password",
+          admin: false
+        },
+        {
+          username: "admin",
+          password: "admin",
+          admin: true
+        },
+      ]
+    }),
+    methods: {
+      login() {
+        let user = this.users.filter( e => this.username === e.username && this.password === e.password);
+        if (user.length === 1 ){
+          console.log("login successfull");
+        } else {
+          this.loginFailedSnackbar.show = true;
+        }
+
+      }
+    }
+  }
+</script>
